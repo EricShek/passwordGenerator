8cba63abda9b983a85ac3616c48e61165f404da9
Eric Shek <ericshek23@gmail.com>
2022-05-20 23:14:31 -0400

    add more rules

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-
+var generateBtn = document.querySelector("#generate")
 var lowercase="abcdefghijklmnopqrstuvwxyz"
 var uppercase= lowercase.toUpperCase()
 var numner="0123456789"
-var special="!@#$%^&*();:,.[{]}=+-_+?><"
+var special="!@#$%^&*();:,.[{]}=+-_?><"
 
 // Write password to the #password input
 function writePassword() {
@@ -20,37 +19,63 @@ function writePassword() {
 //validation numeric
 //validation special characters
 
-function generatePassword(){
+function generatePassword() {
   var finalpassword=""
+
   var size =prompt("Please enter 8 to 128 characters")
   console.log(size)
 
   if (size >= 8 && size <= 128) {
       var isLowercase= confirm("include lowercase?")
       console.log(isLowercase)
+
       var isUppercase= confirm("include uppercase?")
       console.log(isUppercase)
+
       var isNumber= confirm("include number?")
       console.log(isNumber)
+
       var isSpecialcharacter= confirm("include special characters?")
       console.log(isSpecialcharacter)
-
-      if(isLowercase===true){
-        var positionString=Math.floor(Math.random()*26)
-        console.log(positionString)
-        finalpassword=finalpassword+ lowercase[positionString]
-      }
-
   }
-  
+
   else  {
     alert("Invalid entry")
 
   }
 
-  return finalpassword
+      if (isLowercase===true){
+        var positionString=Math.floor(Math.random()*26)
+        console.log(positionString) 
 
+             
+      if(isUppercase===true)
+        var positionString=Math.floor(Math.random()*26)
+        console.log(positionString)
+        
+      if(isNumber===true)
+        var positionString=Math.floor(Math.random()*10)
+        console.log(positionString)
+       
+
+      if(isSpecialcharacter===true)
+        var positionString=Math.floor(Math.random()*25)
+        console.log(positionString)
+
+         finalpassword= finalpassword+ isLowercase[positionString] +isUppercase[positionString]+ isNumber[positionString] + isSpecialcharacter[positionString]
+
+
+         return finalpassword
+        }
+
+ 
+
+  
+  
 }
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
